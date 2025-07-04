9637ee5f4067c2dc8f710308136f224227b021a0
Tobias Bergkvist <tobias@bergkv.ist>
2025-07-04 13:42:38 +0200

    Make thread spawning optional, and improve Uiua exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,36 @@
 #[pyo3::pymodule]
 mod numpy_uiua {
-    use pyo3::exceptions::{PyRuntimeError, PyValueError};
+    use pyo3::create_exception;
+    use pyo3::exceptions::PyException;
     use pyo3::prelude::*;
     use pyo3::types::PyTuple;
     use uiua::{Compiler, SafeSys, Uiua};
 
     use crate::utils::{numpy_array_to_uiua_value, uiua_value_to_numpy_array};
 
+    create_exception!(numpy_uiua, UiuaCompileError, PyException);
+    create_exception!(numpy_uiua, UiuaRuntimeError, PyException);
+
     #[pyclass(name = "uiua")]
     pub struct Program {
         assembly: uiua::Assembly,
+        spawn_threads: bool,
     }
 
     #[pymethods]
     impl Program {
         #[new]
-        pub fn new(src: &str) -> PyResult<Self> {
+        #[pyo3(signature = (src, spawn_threads=false))]
+        pub fn new(src: &str, spawn_threads: bool) -> PyResult<Self> {
             let mut compiler = Compiler::new();
             let assembly = compiler
                 .load_str(src)
-                .map_err(|e| PyValueError::new_err(format!("Uiua compilation error: {e}")))?
+                .map_err(|e| UiuaCompileError::new_err(e.to_string()))?
                 .finish();
-            Ok(Self { assembly })
+            Ok(Self {
+                assembly,
+                spawn_threads,
+            })
         }
 
         #[pyo3(signature = (*args))]
@@ -30,14 +39,17 @@ mod numpy_uiua {
             py: Python<'py>,
             args: Vec<Py<PyAny>>,
         ) -> PyResult<Bound<'py, PyTuple>> {
-            let mut uiua = Uiua::with_backend(SafeSys::with_thread_spawning());
+            let mut uiua = Uiua::with_backend(match self.spawn_threads {
+                true => SafeSys::with_thread_spawning(),
+                false => SafeSys::new(),
+            });
             let inputs = args
                 .into_iter()
                 .map(|x| numpy_array_to_uiua_value(py, &x))
                 .collect::<PyResult<Vec<_>>>()?;
             uiua.push_all(inputs);
             uiua.run_asm(self.assembly.clone())
-                .map_err(|e| PyRuntimeError::new_err(format!("Uiua runtime error: {e}")))?;
+                .map_err(|e| UiuaRuntimeError::new_err(e.to_string()))?;
             let stack = uiua.take_stack();
             let outputs = stack
                 .into_iter()
